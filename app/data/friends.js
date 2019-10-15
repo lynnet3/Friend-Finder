c21844dd74c0ddae01a723d700c40fc16e8cee41
Emily Tucker <emilytucker4@yahoo.com>
2019-10-15 15:57:14 -0400

    added the friends array in friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,3 +1,136 @@
-var people =[
+var people = [{
+        name: "Ahmed",
+        photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        scores: [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+
+    {
+        name: "Milissa",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            5,
+            3,
+            2,
+            5,
+            5,
+            4,
+            5,
+            2,
+            5,
+            5,
+        ]
+    },
     
+    {
+        name: "Asher",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            2,
+            5,
+            1,
+            5,
+            4,
+            5,
+            3,
+            5,
+            5,
+            4,
+        ]
+    },
+    
+    {
+        name: "Julie",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            5,
+            3,
+            5,
+            4,
+            1,
+            3,
+            5,
+            3,
+            5,
+            2,
+        ]
+    },
+    
+    {
+        name: "Jordan",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            4,
+            3,
+            3,
+            2,
+            3,
+            4,
+            5,
+            5,
+            5,
+            1,
+        ]
+    },
+    
+    {
+        name: "Liz",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            5,
+            5,
+            1,
+            5,
+            4,
+            5,
+            5,
+            5,
+            5,
+            5,
+        ]
+    },
+    
+    {
+        name: "Ryan",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores: [
+            4,
+            5,
+            1,
+            4,
+            3,
+            4,
+            4,
+            4,
+            5,
+            5,
+        ]
+    },
+    
+    {
+        name: "Emily",
+        photo: "https://www.google.com/imgres?imgurl=http%3A%2F%2Fcba.ca%2FAssets%2FCBA%2FImages%2FArticle%2Ftiles%2Ftile_women.jpg&imgrefurl=https%3A%2F%2Fcba.ca%2Frepresentation-of-women-at-banks-in-canada&docid=64z_tHfjQf4jJM&tbnid=mah7lYXGPnpYgM%3A&vet=10ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs..i&w=1120&h=720&bih=625&biw=1366&q=woman&ved=0ahUKEwin1I_Ug5_lAhUBZd8KHdHsDGwQMwiGASgLMAs&iact=mrc&uact=8",
+        scores:[
+            2,
+            5,
+            1,
+            5,
+            5,
+            5,
+            2,
+            5,
+            5,
+            5,
+        ]
+    },
 ]
