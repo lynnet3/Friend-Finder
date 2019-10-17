49b8a243bd9062b56b5c4be83e2a8d4608919cf0
Emily Tucker <emilytucker4@yahoo.com>
2019-10-17 13:14:17 -0400

    put in script and added another person in friend.js

---
+++ app/routing/apiRouting.js
@@ -0,0 +1,7 @@
+var peopleList = require("../data/friends");
+
+module.exports = function(app){
+    app.get("/api/people", function(req,res){
+        res.json
+    })
+}
--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var path = require("path");
 var app = express()
 var PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.listen(PORT, function(){
     console.log("We are listening on PORT" + PORT);
 })
