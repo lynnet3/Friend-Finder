52c3f058e8519fe20270ec898b6d8f691e6d1f14
Emily Tucker <emilytucker4@yahoo.com>
2019-12-21 10:59:07 -0500

    fixed overlooked error

--- app/routing/htmlRouting.js
+++ app/routing/htmlRouting.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
     });
 
     app.get("/survey", function (req, res) {
-        res.sendFile(path.join(__dirname, "/../public.survey.html"))
+        res.sendFile(path.join(__dirname, "/../public/survey.html"))
     });
 
     app.get("*", function (req, res) {
